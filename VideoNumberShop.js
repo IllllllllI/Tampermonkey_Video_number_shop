7aeb2da5acd105eed76a81837fea664e2040805b
IllllllllI <zsx_990228_t@petalmail.com>
2023-09-13 03:41:46 +0800

    2

--- VideoNumberShop.js
+++ VideoNumberShop.js
@@ -2,7 +2,7 @@
 // @name         VideoNumberShop
 // @name:zh-CN   视频号小店
 // @namespace    https://blog.chrxw.com
-// @version      1.1
+// @version      1.2
 // @description  快捷添加库存
 // @author       Chr_
 // @match        https://channels.weixin.qq.com/shop/*
@@ -16,6 +16,9 @@ var open_url = null;
     //let open_url = null;
     if(!open_url)
         open_url = open("https://channels.weixin.qq.com/shop/ssr/goods/list?token=2083904492");
+    let btn_weui = document.querySelector("#app button.weui-desktop-btn.weui-desktop-btn_default");
+    let btn_write = document.createElement("button");
+    btn_write.textContent = "写入";
     open_url.document.querySelectorAll("#app button.weui-desktop-btn.weui-desktop-btn_default")[0].click();
         
 })();
