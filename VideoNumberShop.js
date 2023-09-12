09376e02b80b9b738662e8c72ce322afc137b675
IllllllllI <zsx_990228_t@petalmail.com>
2023-09-13 02:43:47 +0800

    modified:   VideoNumberShop.js

--- VideoNumberShop.js
+++ VideoNumberShop.js
@@ -5,33 +5,15 @@
 // @version      1.1
 // @description  去除B站多余的直播播放器（滑稽）
 // @author       Chr_
-// @include      https://live.bilibili.com/*
+// @match        https://channels.weixin.qq.com/shop/*
 // @license      AGPL-3.0
 // @icon         https://blog.chrxw.com/favicon.ico
 // ==/UserScript==
 
 (() => {
     "use strict";
-    //去除播放器的开关
-    let VEnable = window.localStorage.getItem("VEnable") === "true";
-    if (VEnable) {
-        setTimeout(() => {
-            document.getElementById("live-player").remove();
-        }, 3000);
-    }
-    let btnArea = document.querySelector(".right-ctnr");
-    let btn = document.createElement("button");
-    btn.id = "removeLive";
-    btn.textContent = VEnable ? "恢复播放器" : "移除播放器";
-    btn.addEventListener("click", () => {
-        VEnable = !VEnable;
-        window.localStorage.setItem("VEnable", VEnable);
-        btn.textContent = VEnable ? "恢复播放器" : "移除播放器";
-        if (VEnable) {
-            document.getElementById("live-player").remove();
-        } else {
-            window.location.reload();
-        }
-    });
-    btnArea.insertBefore(btn, btnArea.children[0]);
+    let open_url = open("https://channels.weixin.qq.com/shop/ssr/goods/list?token=2083904492");
+    open_url.document.querySelectorAll("#app button.weui-desktop-btn.weui-desktop-btn_default")[0].click();
+        
+    
 })();
