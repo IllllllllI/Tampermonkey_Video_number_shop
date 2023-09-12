1dc235096d976bb617b08d65282ef374d9b20976
IllllllllI <zsx_990228_t@petalmail.com>
2023-09-13 04:09:26 +0800

    5

--- VideoNumberShop.js
+++ VideoNumberShop.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         VideoNumberShop
+// @name         视频号小店
 // @name:zh-CN   视频号小店
 // @namespace    https://blog.chrxw.com
 // @version      1.2
 // @description  快捷添加库存
-// @author       Chr_
+// @author       僵尸先生
 // @match        https://channels.weixin.qq.com/*
 // @license      AGPL-3.0
 // @icon         https://blog.chrxw.com/favicon.ico
