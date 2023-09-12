da2af39d4763fc304970902890d5bbf1c4f1a1fc
IllllllllI <zsx_990228_t@petalmail.com>
2023-09-13 03:22:34 +0800

    1

--- VideoNumberShop.js
+++ VideoNumberShop.js
@@ -1,19 +1,21 @@
 // ==UserScript==
-// @name         Remove_Live
-// @name:zh-CN   去除B站直播间播放器
+// @name         VideoNumberShop
+// @name:zh-CN   视频号小店
 // @namespace    https://blog.chrxw.com
 // @version      1.1
-// @description  去除B站多余的直播播放器（滑稽）
+// @description  快捷添加库存
 // @author       Chr_
 // @match        https://channels.weixin.qq.com/shop/*
 // @license      AGPL-3.0
 // @icon         https://blog.chrxw.com/favicon.ico
 // ==/UserScript==
 
+var open_url = null;    
 (() => {
     "use strict";
-    let open_url = open("https://channels.weixin.qq.com/shop/ssr/goods/list?token=2083904492");
+    //let open_url = null;
+    if(!open_url)
+        open_url = open("https://channels.weixin.qq.com/shop/ssr/goods/list?token=2083904492");
     open_url.document.querySelectorAll("#app button.weui-desktop-btn.weui-desktop-btn_default")[0].click();
         
-    
 })();
