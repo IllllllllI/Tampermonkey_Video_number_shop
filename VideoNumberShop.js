ce2531a6c38f5763539591b11cca74d215172e8e
IllllllllI <zsx_990228_t@petalmail.com>
2023-09-13 04:19:13 +0800

    1

--- VideoNumberShop.js
+++ VideoNumberShop.js
@@ -13,21 +13,22 @@
 (() => {
     "use strict";
     // 监听键盘按下事件
+    let open_url = null
     document.addEventListener('keydown', function(event) {
         // 判断按下的是哪个键
-        if (event.key === 'go doc start') {
+        if (event.key === 'Home') {
             // 执行特定的操作或功能
-            let open_url = open("https://channels.weixin.qq.com/shop/ssr/goods/list?token=2083904492");
-            console.log('按下了a键');
+            open_url = open("https://channels.weixin.qq.com/shop/ssr/goods/list?token=2083904492");
+            console.log('按下了a键')
         } else if (event.key === 'b') {
-            console.log('按下了b键');
+            console.log('按下了b键')
     }
     // 可以根据需要添加更多的按键映射
   });
-  
+
     let btn_weui = document.querySelector("#app button.weui-desktop-btn.weui-desktop-btn_default");
     let btn_write = document.createElement("button");
-    btn_write.textContent = "写入";
+    btn_write.textContent = "写入"
     open_url.document.querySelectorAll("#app button.weui-desktop-btn.weui-desktop-btn_default")[0].click();
-        
+
 })();
