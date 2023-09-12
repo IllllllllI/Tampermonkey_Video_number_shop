ff7e69ad4d28284131db49cdc634b203022423d5
IllllllllI <zsx_990228_t@petalmail.com>
2023-09-13 01:53:53 +0800

    modified:   VideoNumberShop.js

--- VideoNumberShop.js
+++ VideoNumberShop.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Video number script
-// @namespace    none
+// @name         视频号脚本
+// @namespace    https://greasyfork.org/zh-CN/users/1171770-illlllllli
 // @version      0.1
 // @description  new script
 // @author       You
-// @match        *://*
+// @match        https://channels.weixin.qq.com/shop
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @license AGPL-3.0
 // @grant        none
